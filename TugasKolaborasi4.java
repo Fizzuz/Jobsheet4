824a85389305faba383963267db6075d30e2b3dc
Rose <crowsquare02@gmail.com>
2025-10-01 19:41:02 +0700

    commit

--- TugasKolaborasi4.java
+++ TugasKolaborasi4.java
@@ -1,7 +1,8 @@
-public class TugasKolaborasi17{
+public class TugasKolaborasi4{
     public static void main(String[] args) {
         System.out.println("Haii Aku hafiz ");
         System.out.println("-------------------");
         System.out.println("Testinggg");
+        System.out.println("ini pembajakan");
     }
 }
