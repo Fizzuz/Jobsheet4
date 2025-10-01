6380f85e3314deac6769229fe91455ca646ff6b0
Fizzuz <mhafizgpr@gmail.com>
2025-10-01 19:36:28 +0700

    commit

--- TugasKolaborasi17.java
+++ TugasKolaborasi17.java
@@ -2,5 +2,6 @@ public class TugasKolaborasi17{
     public static void main(String[] args) {
         System.out.println("Haii Aku hafiz ");
         System.out.println("-------------------");
+        System.out.println("Testinggg");
     }
 }
